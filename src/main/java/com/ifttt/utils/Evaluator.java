1cd4def619a7dc897fa6424015c892c0d2b901bc
pk1992 <praveen.k@gmail.com>
2018-07-15 16:31:40 +0530

    removed unused imports

--- src/main/java/com/ifttt/utils/Evaluator.java
+++ src/main/java/com/ifttt/utils/Evaluator.java
@@ -1,7 +1,6 @@
 package com.ifttt.utils;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.BooleanNode;
 import com.ifttt.enums.OperationTypeEnum;
 import com.ifttt.enums.OperatorEnum;
