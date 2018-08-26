2393a89ca2f72aef85cb7fe120708e46ed669ffd
pk1992 <praveen.k@gmail.com>
2018-08-26 13:34:03 +0530

    implemented dynamic janino variable types

--- src/main/java/com/ifttt/utils/JaninoExpressUtils.java
+++ src/main/java/com/ifttt/utils/JaninoExpressUtils.java
@@ -1,8 +1,8 @@
 package com.ifttt.utils;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.ifttt.enums.ExpressParamTypeEnum;
 import org.codehaus.janino.ExpressionEvaluator;
-
-import java.util.Arrays;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -13,18 +13,26 @@ public class JaninoExpressUtils {
 
     private static final Map<String, Object> compiledExpressions = new ConcurrentHashMap<>();
 
-    public static Object brew(String key, String expression, String variables[], Object substitutes[]) throws Exception {
+    public static Object brew(String key, String expression, String variables[], JsonNode paramTypes, Object substitutes[]) throws Exception {
         if(compiledExpressions == null) {
             return null;
         }
         ExpressionEvaluator evaluator = (ExpressionEvaluator) compiledExpressions.get(key);
         if(evaluator == null) {
-            System.out.println("Adding janino evaluator for key :: " + key);
+            Class[] clazzes = getParamTypes(variables, paramTypes);
             evaluator = new ExpressionEvaluator();
-            evaluator.setParameters(variables, new Class[] { int.class, int.class });
+            evaluator.setParameters(variables, clazzes);
             evaluator.cook(expression);
             compiledExpressions.put(key, evaluator);
         }
         return evaluator.evaluate(substitutes);
+    }
+
+    private static Class[] getParamTypes(String[] variables, JsonNode paramTypes) {
+        Class clazzes[] = new Class[variables.length];
+        for (int index = 0; index < variables.length; index++) {
+            clazzes[index] = ExpressParamTypeEnum.valueOf(paramTypes.get(variables[index]).asText()).getClazz();
+        }
+        return clazzes;
     }
 }
